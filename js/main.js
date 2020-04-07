38ce8c11e6d00816c16a58bdb2e5d62661484d06
swapnil <reach_swapnil@outlook.com>
2020-04-07 23:15:24 +0530

    Working

--- js/main.js
+++ js/main.js
@@ -28,7 +28,50 @@ function saveBookmark(e){
 
     
   }
-
+  fetchBookmarks();
 
   e.preventDefault();
+}
+
+//delete bookmark
+function deleteBookmark(url){
+  //get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //loop through
+  for(var i= 0;i<bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      bookmarks.splice(i,1);
+
+    } 
+   }
+   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  //refetch
+  fetchBookmarks();
+
+}
+
+
+
+//fetch bookmarks
+
+function fetchBookmarks(){
+  //get array from local storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  
+  var bookmarksResults = document.getElementById('bookmarksResults');
+  bookmarksResults.innerHTML='';
+
+  for(var i=0;i<bookmarks.length;i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url; 
+    bookmarksResults.innerHTML += '<div class = "well">' +
+                                     '<h3>' + name + 
+                                     ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' + 
+                                     ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>' + 
+                                     '</h3>' + 
+                                     '</div>';
+
+
+  }
 }
